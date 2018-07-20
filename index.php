c95579198fa3e872c37ba5b4a3622d5ce4d898da
Ronald P Mathews <ronaldpmathews@gmail.com>
2018-07-20 19:38:15 +0530

    First push
    Initial file push

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+  <head>
+    <title>PagePod | News sharing platform</title>
+    <link rel='stylesheet' href='assets/css/main.css'>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+  </head>
+  <body>
+    <div class='indexTopNav'>
+      <img src='assets/img/logo.png' class='logo'>
+      <br />
+      <div class='navLinks'>
+        <div class='navElem'>HOME</div>
+        <div class='navElem'>SELECTION-A</div>
+        <div class='navElem'>SELECTION-B</div>
+        <div class='navElem'>SELECTION-C</div>
+        <div class='navElem'>SELECTION-D</div>
+      </div>
+    </div>
+  </body>
+</html>
