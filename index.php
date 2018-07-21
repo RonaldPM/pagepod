9a4f699cd40a5d0bbc5d81f571c4f16f214cf909
Ronald P Mathews <ronaldpmathews@gmail.com>
2018-07-21 10:06:53 +0530

    Added comments

--- index.php
+++ index.php
@@ -1,14 +1,21 @@
+<!--
+  DEVELOPMENT STARTED ON : 21-JULY-2018
+  REPOSITORY NAME        : PAGEPOD
+  REPOSITORY LINK        : https://github.com/RonaldPM/pagepod
+  DEVELOPER email        : ronaldpmathews@gmail.com
+-->
 <html>
   <head>
     <title>PagePod | News sharing platform</title>
     <link rel="shortcut icon" type="image/png" href="assets/img/logo.png"/>
     <link rel='stylesheet' href='assets/css/main.css'>
     <script type='text/javascript' src='assets/js/ui.js'></script>
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600" rel="stylesheet"><!--GOOGLE FONTS API-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"><!--FOR GOOGLE MATERIAL ICONS-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   </head>
   <body>
+    <!--TOP NAVIGATION BAR FOR THE INDEX PAGE. THIS CONTAINS LOGO, LINKS etc.-->
     <div class='indexTopNav'>
       <div class='navTop'>
         <img src='assets/img/logo.png' class='logo' title='PagePod'>
@@ -29,13 +36,18 @@
         <button class='socialBtn'>Twitter</button>
         <button class='socialBtn'>Facebook</button>
         <button class='socialBtn'>Newsletter</button>
+        <!--SEARCH BUTTON AND INPUT CODE STARTS HERE.-->
         <div class='searchBar'>
           <input type='text' class='navSearchInp' placeholder="Search pagePod..." id='searchInp'>
           <i class="material-icons navSearchBtn" onclick="showSearch()">search</i>
         </div>
+        <!--SEARCH BUTTON AND INPUT CODE ENDS HERE.-->
       </div>
     </div>
+    <!--TOP NAVIGATION BAR ENDS HERE-->
+    <!--BODY OF THE PAGE STARTS HERE-->
     <div class='bodyWrap'>
+      <!--BOX FOR LATEST ARTICLE STARTS HERE-->
       <div class='latestBox'>
         <img src='media/img/duckduckgo.jpg' class='latestImgHolder'>
         <div class='latestTitle'>
@@ -46,6 +58,8 @@
         <br /><br />
         <button class='tag'>LATEST</button>
       </div>
+      <!--BOX FOR LATEST ARTICLE ENDS HERE-->
     </div>
+    <!--BODY OF THE PAGE ENDS HERE-->
   </body>
 </html>
